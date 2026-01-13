7c67447fe793d1d0857b3e32dc0b1e1773db4e9f
Gautam Singh <gautamsingh@Gautams-MacBook-Air.local>
2026-01-13 07:07:41 +0530

    added schema

---
+++ db.js
@@ -0,0 +1,45 @@
+const {Schema, default: mongoose}=require("mongoose");
+console.log("Mongoose called to called");
+mongoose.connect("mongodb+srv://gautamsingh6006218_db_user:Singhautam1@cluster0.tqjgxxd.mongodb.net/coursera-app");
+const ObjectId=mongoose.Types.ObjectId;
+
+
+const userSchema=new Schema({
+    email: {type: String, unique: true},
+    password: String,
+    firstName: String,
+    lastName: String
+
+});
+const adminSchema=new Schema({
+    email: {type: String, unique: true},
+    password: String,
+    firstName: String,
+    lastName: String
+})
+const courseSchema=new Schema({
+    title: String,
+    description: String,
+    price: Number,
+    imageUrl: String,
+    creatorId: ObjectId
+})
+const purchaseSchema=new Schema({
+    userId: ObjectId,
+    courseId: ObjectId
+
+})
+
+const userModel=mongoose.model("user",userSchema);
+const adminModel=mongoose.model("admin",adminSchema);
+const courseModel=mongoose.model("course",courseSchema);
+const purchaseModel=mongoose.model("purchase",purchaseSchema);
+
+module.exports={
+    userModel,
+    adminModel,
+    courseModel,
+    purchaseModel
+}
+
+
