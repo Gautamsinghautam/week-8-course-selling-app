2ab2fe2f8a6e8935294aa7d97e81496948ab6249
Gautam Singh <gautamsingh@Gautams-Air.bbrouter>
2026-01-18 12:13:48 +0530

    added user and admin endpoints

--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,46 @@
 const {Router}=require("express");
 const adminRouter=Router();
+const jwt=require("jsonwebtoken");
 const {adminModel}=require("../db");
+const JWT_ADMIN_PASSWORD="GautamAdimin";
 
 // adminRouter.use(adminMiddleware);
 //passsowrd hashing libraray Bcrypt ZOD(for ipn validation), json token for token generatioon
-adminRouter.post("/signup",function(req,res){
+adminRouter.post("/signup",async function(req,res){
     
-
-    res.json({message: "signup endpoint"});
+const {email, password, firstname, lastName} = req.body; //todo: adding zod validation
+//TODO: has the password so plaintex  pw is not stored in DB
+//TODOD; put inside trycatch
+try{
+    await adminModel.create({
+        email: email,
+        password: password,
+        firstname: firstname,
+        lastName: lastName
+    })
+    }catch(e){
+        console.log("ur signup failed"+ e);
+    }
+    res.json({message: "signup endpoint"})
 })
 
-adminRouter.post("/signin",function(req,res){
+adminRouter.post("/signin",async function(req,res){
 
-    res.json({message: "sign in endpoint postEndpoint"});
+    const {email, password}=req.body;
+    //TODO: ideally passsword syhou,d be hashed so that passowrd should be 
+    const admin=await adminModel.findOne({
+        email: email,
+        password: password
+    });
+    if(admin){
+        //generate token and send
+        const token=jwt.sign({id: admin._id},JWT_ADMIN_PASSWORD);
+        //todo Do cookie logibc
+        res.json({token: token});
+    }else{
+        res.status(403).json({message: "incorrect credentials"});
+    }
+
 })
 adminRouter.post("/course",function(req,res){
 
--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,43 @@
 // const express=require("express");
 const { Router}=require("express");
 const userRouter=Router();
+const {userModel}=require("../db");
+const jwt=require("jsonwebtoken");
+const JWT_USER_PASSWORD="gautam12345"; //todo : put in env variable
 
-
-userRouter.post("/signup",function(req,res){
-
+userRouter.post("/signup",async function(req,res){
+    const {email, password, firstname, lastName} = req.body; //todo: adding zod validation
+//TODO: has the password so plaintex  pw is not stored in DB
+//TODOD; put inside trycatch
+try{
+    await userModel.create({
+        email: email,
+        password: password,
+        firstname: firstname,
+        lastName: lastName
+    })
+    }catch(e){
+        console.log("ur signup failed"+ e);
+    }
     res.json({message: "signup endpoint"})
 })
 
-userRouter.post("/signin",function(req,res){
+userRouter.post("/signin",async function(req,res){
+    const {email, password}=req.body;
+    //TODO: ideally passsword syhou,d be hashed so that passowrd should be 
+    const user=await userModel.findOne({
+        email: email,
+        password: password
+    });
+    if(user){
+        //generate token and send
+        const token=jwt.sign({id: user._id},JWT_USER_PASSWORD);
+        //todo Do cookie logibc
+        res.json({token: token});
+    }else{
+        res.status(403).json({message: "incorrect credentials"});
+    }
 
-    res.json({message: "Signin endpoint"})
 })
 
 userRouter.get("/purchange",function(req,res){
